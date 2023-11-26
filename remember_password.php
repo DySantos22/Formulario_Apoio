23ee82a88eca6313aa313fdae9f15d51b0c635e9
DySantos22 <dyllanlima17@gmail.com>
2023-11-26 20:01:30 -0300

    Envio de Email

--- remember_password.php
+++ remember_password.php
@@ -13,11 +13,17 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Pegando Chave de confirmação e email
-$chave = $_SESSION['chave'];
-$email = $_SESSION['email'];
+if(isset($_POST['email_lembrar'])){
 
+//Pegando email do usuario 
+$email = $_POST['email_lembrar'];
 
+//Buscando no BD
+$sql = "SELECT * FROM usuario WHERE Email = '$email'";
+    //Procurando o resultado
+    $result = $conn->query($sql);
+    //Se houver linha no BD
+    if($result->num_rows> 0){
 
 //Instanciando a váriavel do email  
 $mail = new PHPMailer(true);     //Instancia do PHPmailer
@@ -30,8 +36,8 @@ try {
     $mail->SMTPSecure = 'tls';                                  //Enviar usando TLS
     $mail->Host       = 'smtp.gmail.com';                     //Servidor usado
     $mail->SMTPAuth   = true;                                   //Ativando autenticacao SMTP
-    $mail->Username   = '';                     //Usuario SMTP
-    $mail->Password   = '';                        //Senha SMTP     
+    $mail->Username   = 'dyllanlima17@gmail.com';                     //Usuario SMTP
+    $mail->Password   = 'dvjz wisa bcdx xoax';                        //Senha SMTP     
     $mail->Port       = 587;        //Porta usada para TLS
 
     //Aqui ele tira o erro do SSL e da conexão com o Host
@@ -44,41 +50,36 @@ try {
     );
     
     //quem envia e recebe
-    $mail->setFrom('', '');  //Usuario SMTP e Nome aleatório
+    $mail->setFrom('dyllanlima17@gmail.com', 'Dyllan Lima');  //Usuario SMTP e Nome aleatório
     $mail->addAddress($email);     //Email do Destinatario
     $mail->isHTML(true);                                  //Habilitando o uso do HTML
     $mail->charset = 'UTF-8';
-    $mail->Subject = 'Definir uma Senha';    //Titulo
-    $mail->Body    = "Olá, seja bem vindo!!<br><br>Obrigado pelo seu apoio!!<br><br>Clique no link abaixo para  cadastrar sua senha:<br><br>
-    <a href='http://localhost/editpassword.php?email=$email' target='_blank'>Clique Aqui!</a>";   //Corpo
-    $mail->AltBody = "Olá, seja bem vindo!!\n\nObrigado pelo seu apoio!!\n\nClique no link abaixo para cadastrar sua senha:\n\n
-    <a href='http://localhost/editpassword.php?email=$email' target='_blank'>Clique Aqui!</a>";
+    $mail->Subject = 'Troca de Senha';    //Titulo
+    $mail->Body    = "Olá, Tudo bem?<br><br>Recebemos aqui seu pedido para a troca de sua senha!<br><br>Clique no link abaixo para realizar a troca da senha: <br><br>
+    <a href='http://localhost/remember_password2.php?email=$email' target='_blank'>Clique Aqui!</a>";   //Corpo
+    $mail->AltBody = "Olá, Tudo bem?\n\nRecebemos aqui seu pedido para a troca de sua senha!\n\nClique no link abaixo para realizar a troca da senha:\n\n
+    <a href='http://localhost/remember_password2.php?email=$email' target='_blank'>Clique Aqui!</a>";
   
     //Verificando envio do email
     if($mail->send()) {
-        header('refresh:4;url=index.html');
+        include 'boxes/sucessremember.html';
+        header('refresh:4;url=login.html');
     } else {
-        header('location: index.html');
+        header('location: login.html');
         }
 
 } catch (Exception $e) {
     echo "Mensagem não foi enviada. ERRO: {$mail->ErrorInfo}";   //Mensagem de erro, depois envia para o cadastro novamente
-    header('location: index.html');
+    header('location: login.html');
 }
-
+    }else{
+        include 'boxes/inexistemail.html';
+        header('refresh:4;url=login.html');
+    }
+}else{
+    header('location: login.html');
+}
 //Mandando os dados para o banco 
 mysqli_close($conn);
 
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-<script src="js/sweetalert2.js"></script>
-<script src="js/custom.js"></script>
-</body>
-</html>
